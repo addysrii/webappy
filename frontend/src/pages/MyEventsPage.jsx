b0a13456e87c44b86e0057b3a0baf95b7878ef57
addysrii <149177246+addysrii@users.noreply.github.com>
2025-08-10 20:42:34 +0530

    Update MyEventsPage.jsx

--- frontend/src/pages/MyEventsPage.jsx
+++ frontend/src/pages/MyEventsPage.jsx
@@ -110,7 +110,7 @@ const MyEventsPage = () => {
   };
   
   return (
-    <div className="flex flex-col md:flex-row h-screen bg-orange-50">
+    <div className="flex flex-col md:flex-row h-screen top-8 bg-green-50">
       {/* Sidebar - hidden on mobile, visible on md and up */}
       <div className="hidden md:block">
         <Sidebar user={user} />
